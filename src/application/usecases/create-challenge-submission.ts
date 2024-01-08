8763daa7bded3b10701c84722425999178984f5e
myguelangello <myguelangello624@gmail.com>
2024-01-07 23:30:17 -0300

    feat(usecases): Add data validation of repositories when creating the challenge

--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,4 +1,6 @@
 import { Submission } from "../../domain/entities/submission"
+import { ChallengesRepository } from "../repositories/ChallengesRepository";
+import { StudentsRepository } from "../repositories/StudentsRepository";
 
 type CreateChallengeSubmissionRequest = {
   studentId: string
@@ -6,7 +8,27 @@ type CreateChallengeSubmissionRequest = {
 }
 
 export class CreateChallengeSubmission {
-  execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) { // esse método basicamente a única funcão de criação/submissão de um desafio
+  /**
+   * caso com eslint não funcionar pode criar com private fora do constructor, passar o parâmetro e dentro chamar com "this."
+   */
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private challengesRepository: ChallengesRepository,
+  ) { }
+
+  async execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) { // esse método basicamente a única funcão de criação/submissão de um desafio
+    const student = await this.studentsRepository.findById(studentId);
+
+    if (!student) {
+      throw new Error("Student does not exists.")
+    }
+
+    const challenge = await this.challengesRepository.findById(challengeId);
+
+    if (!challenge) {
+      throw new Error("Challenge does not exists.")
+    }
+
     const submission = Submission.create({
       studentId,
       challengeId,
