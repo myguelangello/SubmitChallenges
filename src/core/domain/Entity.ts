7fa6be22e5802eec8f730b997a4514bef0d1631f
myguelangello <myguelangello624@gmail.com>
2024-01-07 21:11:02 -0300

    feat: create super class(abstract) Entity

---
+++ src/core/domain/Entity.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from 'node:crypto'
+export abstract class Entity<T> {
+  protected _id: string
+  public props: T // esse T indica uqe o tipo de props pode ser genérico
+
+  /**
+   * 
+   * @param props são todos os campos que a classe que herda possui
+   */
+  constructor(props: T, id?: string) {
+    this.props = props
+    this._id = id ?? randomUUID()
+  }
+}
+/**
+ * Essa classe implementa comportamentos padrões entre todas as classes (como as entidades em entities)
+ */
