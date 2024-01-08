2a039413e567fdd22d2a357a3ef311ff36d36ddb
myguelangello <myguelangello624@gmail.com>
2024-01-07 23:25:12 -0300

    feat(Entity): Add get id method

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -3,6 +3,10 @@ export abstract class Entity<T> {
   protected _id: string
   public props: T // esse T indica uqe o tipo de props pode ser genérico
 
+  get id() {
+    return this._id
+  }
+
   /**
    * 
    * @param props são todos os campos que a classe que herda possui
